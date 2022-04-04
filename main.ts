03e7b6883fc8add08054f2ad232a7f447f68ba3f
jazibghori <77227927+jazibghori@users.noreply.github.com>
2022-04-04 14:51:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+let NUMBER = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    NUMBER = randint(1, 6)
+    if (NUMBER == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (NUMBER == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (NUMBER == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (NUMBER == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (NUMBER == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
